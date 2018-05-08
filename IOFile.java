54d2ab603f8b1434c21c2c52880dc1286e2479ae
Trần Thiện Dinh <30461218+thiendinhdk@users.noreply.github.com>
2018-05-08 22:00:37 +0700

    Update IOFile.java

--- IOFile.java
+++ IOFile.java
@@ -1,114 +1,58 @@
-package Model;
+package Controller;
 
-import java.util.ArrayList;
-
+import Model.ChuDe;
+import Model.DuLieu;
+import Model.TuVung;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 /**
- * class : quản lí các chủ đề 
+ * class : quản lí đọc đầu vào
  * author Dinh Tran
  */
-public class ChuDe {
+public class IOFile {
 
-    private ArrayList<TuVung> danhSachTu = new ArrayList(); // tạo 1 danh sách từ
-    private String tenChuDe; // tạo tên chủ đề
-    private int soLuongTu; // tổng số từ trong chủ đề
-    private String linkFile; // link tìm đến file chủ đề
-
-    public ChuDe() {
+    /**
+     * lấy tên chủ đề nạp vào dữ liệu
+     */
+    public final static String path = "Data\\";
+    public static void inputChuDe(DuLieu data) {
+        File file = new File(path + "listTopic.txt");
+        try {
+            Scanner sc = new Scanner(file);
+            while (sc.hasNext()) {
+                ChuDe temp = new ChuDe();
+                temp.setTenChuDe(sc.nextLine());
+                temp.setLinkFile(path + sc.nextLine());
+                data.addChuDe(temp);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("có lỗi");
+        }
     }
 
     /**
-     * hàm khởi tạo 1 chủ đề
+     * lấy từ vựng nạp vào chủ đề , rồi nạp vào dữ liệu
      *
-     * @param tenChuDe
-     * @param linkFile
-     * @param soLuongTu
+     * @param data
      */
-    public ChuDe(String tenChuDe, String linkFile, int soLuongTu) {
-        this.tenChuDe = tenChuDe;
-        this.soLuongTu = soLuongTu;
-        this.linkFile = linkFile;
-    }
-
-    /**
-     * lấy danh sách từ
-     *
-     * @return danh sách từ
-     */
-    public ArrayList<TuVung> getDanhSachTu() {
-        return danhSachTu;
-    }
-
-    /**
-     * thiết lập danh sách từ
-     *
-     * @param danhSachTu
-     */
-    public void setDanhSachTu(ArrayList<TuVung> danhSachTu) {
-        this.danhSachTu = danhSachTu;
-    }
-
-    /**
-     * lấy tên chủ đề
-     *
-     * @return tên chủ đề
-     */
-    public String getTenChuDe() {
-        return tenChuDe;
-    }
-
-    /**
-     * thiết lập tên chủ đề
-     *
-     * @param tenChuDe
-     */
-    public void setTenChuDe(String tenChuDe) {
-        this.tenChuDe = tenChuDe;
-    }
-
-    /**
-     * lấy số lượng từ
-     *
-     * @return số lượng từ
-     */
-    public int getSoLuongTu() {
-        return soLuongTu;
-    }
-
-    /**
-     * thiết lập số lượng từ
-     *
-     * @param soLuongTu
-     */
-    public void setSoLuongTu(int soLuongTu) {
-        this.soLuongTu = soLuongTu;
-    }
-
-    /**
-     * lấy đường dẫn file
-     *
-     * @return đường dẫn file
-     */
-    public String getLinkFile() {
-        return linkFile;
-    }
-
-    /**
-     * thiết lập đường dẫn file
-     *
-     * @param linkFile
-     */
-    public void setLinkFile(String linkFile) {
-        this.linkFile = linkFile;
-    }
-
-    /**
-     * thêm 1 từ vào chủ đề
-     *
-     * @param tuMoi
-     */
-    public void addTu(TuVung tuMoi) {
-        danhSachTu.add(tuMoi);
-        soLuongTu++;
+    public static void inputTuVung(DuLieu data) {
+        for (int i = 0; i < data.getSoLuongChuDe(); i++) {
+            File file = new File(data.getDanhSachChuDe().get(i).getLinkFile());
+            try {
+                Scanner sc = new Scanner(file);
+                while (sc.hasNext()) {
+                    TuVung temp = new TuVung();
+                    temp.setTenTu(sc.nextLine());
+                    temp.setPhienAm(sc.nextLine());
+                    temp.setTuLoai(sc.nextLine());
+                    temp.setNghia(sc.nextLine());
+                    temp.setLinkImage(path + "image\\" + sc.nextLine());
+                    data.getDanhSachChuDe().get(i).addTu(temp);
+                }
+            } catch (FileNotFoundException e) {
+            }
+        }
     }
 
 }
