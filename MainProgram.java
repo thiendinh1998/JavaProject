ea9523d5878b208cf490602ba0130d3a01d97bf6
Trần Thiện Dinh <30461218+thiendinhdk@users.noreply.github.com>
2018-05-09 11:49:36 +0700

    Update MainProgram.java

--- MainProgram.java
+++ MainProgram.java
@@ -3,23 +3,28 @@ package View;
 import Controller.IOFile;
 import Model.BienToanCuc;
 import Model.ChuDe;
+import View.HocTuVung;
+import View.OnTapTuVung;
+import View.ThongKeDuLieu;
+import View.TracNghiemOnTap;
+import View.WordComparator;
+import java.awt.Dimension;
+import java.awt.Graphics;
 import java.util.Collections;
 import java.util.Iterator;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 
-/**
- * class : đại diện cho cửa sổ ban đầu khi mở chương trình
- */
+
 public class MainProgram extends javax.swing.JFrame {
 
-    /**
-     * Creates new form MainProgram
-     */
+    
     public MainProgram() {
-        initComponents();    
+        initComponents();// phương thức định nghĩa giao diện cho Mainprogram    
     }
 
-    @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
     private void initComponents() {
 
         jButton1 = new javax.swing.JButton();
@@ -29,6 +34,15 @@ public class MainProgram extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         jButton6 = new javax.swing.JButton();
         jButton7 = new javax.swing.JButton();
+        jLayeredPane1 = new javax.swing.JLayeredPane(){
+            ImageIcon icon = new ImageIcon("gh.jpg");
+            public void paintComponent(Graphics g){
+                Dimension d = getSize();
+                g.drawImage(icon.getImage(), 0, 0, d.width, d.height, null);
+                setOpaque(false);
+                super.paintComponent(g);
+            }
+        };
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setLocation(new java.awt.Point(400, 100));
@@ -55,11 +69,13 @@ public class MainProgram extends javax.swing.JFrame {
             }
         });
 
-        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
+        jLabel1.setFont(new java.awt.Font("Times New Roman", 1, 24)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 0, 0));
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
         jLabel1.setText("LEARNING ENGLISH");
 
-        jLabel2.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        jLabel2.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(204, 204, 0));
         jLabel2.setText("Tùy chọn");
 
         jButton6.setText("TRẮC NGHIỆM");
@@ -76,36 +92,51 @@ public class MainProgram extends javax.swing.JFrame {
             }
         });
 
+        javax.swing.GroupLayout jLayeredPane1Layout = new javax.swing.GroupLayout(jLayeredPane1);
+        jLayeredPane1.setLayout(jLayeredPane1Layout);
+        jLayeredPane1Layout.setHorizontalGroup(
+            jLayeredPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 577, Short.MAX_VALUE)
+        );
+        jLayeredPane1Layout.setVerticalGroup(
+            jLayeredPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 445, Short.MAX_VALUE)
+        );
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(58, 58, 58)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel2)
                     .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jButton7, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(74, 74, 74)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(jButton6, javax.swing.GroupLayout.DEFAULT_SIZE, 103, Short.MAX_VALUE)
-                            .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 1, Short.MAX_VALUE))
-                        .addGap(61, 61, 61)
-                        .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 99, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(63, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 319, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(90, 90, 90))
+                        .addGap(58, 58, 58)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(74, 74, 74)
+                                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 113, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(62, 62, 62)
+                                .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 99, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(jLabel2)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(82, 82, 82)
+                                .addComponent(jButton7, javax.swing.GroupLayout.PREFERRED_SIZE, 113, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(81, 81, 81)
+                                .addComponent(jButton6))))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(176, 176, 176)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 285, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(68, Short.MAX_VALUE))
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addComponent(jLayeredPane1, javax.swing.GroupLayout.Alignment.TRAILING))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(55, 55, 55)
+                .addGap(62, 62, 62)
                 .addComponent(jLabel1)
-                .addGap(43, 43, 43)
+                .addGap(36, 36, 36)
                 .addComponent(jLabel2)
                 .addGap(36, 36, 36)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
@@ -116,96 +147,62 @@ public class MainProgram extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton7, javax.swing.GroupLayout.PREFERRED_SIZE, 67, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jButton6, javax.swing.GroupLayout.PREFERRED_SIZE, 67, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(74, Short.MAX_VALUE))
+                .addContainerGap(75, Short.MAX_VALUE))
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addComponent(jLayeredPane1, javax.swing.GroupLayout.Alignment.TRAILING))
         );
 
         pack();
-    }// </editor-fold>//GEN-END:initComponents
-    /**
-     * đóng cửa sổ chính , mở cửa sổ tra cứu từ vựng
-     */
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        new TraCuuTuVung().setVisible(true);
-        this.dispose();
-    }//GEN-LAST:event_jButton1ActionPerformed
-    /**
-     * đóng cửa sổ chính , mở cửa sổ HỌC TỪ vựng 
-     */
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        new HocTuVung().setVisible(true);
-        this.dispose();
-    }//GEN-LAST:event_jButton2ActionPerformed
+    }// </editor-fold>                        
+    
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {                                         
+        new TraCuuTuVung().setVisible(true); // mở của sổ tra cứu từ vựng,setvisable định nghĩa việc ẩn hiện của JPanel
+        this.dispose();                      // đóng cửa sổ chính
+    }                                        
+   
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {                                         
+        new HocTuVung().setVisible(true); // mở cửa sổ học từ vựng
+        this.dispose();                   //đóng cửa sổ chính
+    }                                        
 
-   /**
-     * đóng cửa sổ chính , mở thống kê dữ liệu
-     * @param evt 
-     */
-    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-        new ThongKeDuLieu().setVisible(true);
-    }//GEN-LAST:event_jButton4ActionPerformed
-    /**
-     * đóng cửa sổ chính , mở cửa sổ trắc nghiệm ôn tập
-     * @param evt 
-     */
-    private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
-        new TracNghiemOnTap().setVisible(true);
-        this.dispose();
-    }//GEN-LAST:event_jButton6ActionPerformed
+   
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {                                         
+        new ThongKeDuLieu().setVisible(true); // mở của sổ thống kê dữ liệu
+    }                                        
+   
+    private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {                                         
+        new TracNghiemOnTap().setVisible(true); //mở cửa sổ trắc nghiệm ôn tập
+        this.dispose();                         //đóng cửa sổ chính
+    }                                        
 
-   /**
-     * đóng cửa sổ chính , mở ôn tập từ vựng
-     * @param evt 
-     */
-    private void jButton7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton7ActionPerformed
-        new OnTapTuVung().setVisible(true);
-        this.dispose();
-    }//GEN-LAST:event_jButton7ActionPerformed
+   
+    private void jButton7ActionPerformed(java.awt.event.ActionEvent evt) {                                         
+        new OnTapTuVung().setVisible(true);//mở ôn tập từ vựng
+        this.dispose();//đóng cửa sổ chính
+    }                                        
 
-    /**
-     * hàm main chính chạy chương trình
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(MainProgram.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(MainProgram.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(MainProgram.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(MainProgram.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
+   
+    public static void main(String args[]) { //hàm main chính chạy chương trình
+    
+        // tạo và chạy form ban đầu
+        java.awt.EventQueue.invokeLater(new Runnable() {   
             public void run() {
                 new MainProgram().setVisible(true);
                 
             }
         });
+        
         // khởi tạo dữ liệu cho chủ dề 
         IOFile.inputChuDe(BienToanCuc.duLieu1);
         IOFile.inputTuVung(BienToanCuc.duLieu1);
         // sắp xếp các từ
         for( ChuDe cd : BienToanCuc.duLieu1.getDanhSachChuDe() ) {
             Collections.sort(cd.getDanhSachTu(),new WordComparator());  
-            Iterator wtf =cd.getDanhSachTu().iterator(); 
+            Iterator hihi =cd.getDanhSachTu().iterator(); 
         }
     }
 
-    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // Variables declaration - do not modify                     
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton4;
@@ -213,5 +210,7 @@ public class MainProgram extends javax.swing.JFrame {
     private javax.swing.JButton jButton7;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    // End of variables declaration//GEN-END:variables
+    private javax.swing.JLayeredPane jLayeredPane1;
+    // End of variables declaration                   
+
 }
